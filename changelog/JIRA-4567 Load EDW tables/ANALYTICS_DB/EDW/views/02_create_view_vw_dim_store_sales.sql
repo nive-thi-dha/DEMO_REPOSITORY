23c02c4f31c334f7f83e877a224338d9e6a13997
nive-thi-dha <Nivethidha.S@gds.ey.com>
2025-02-07 17:42:04 +0530

    Update 02_create_view_vw_dim_store_sales.sql

--- changelog/JIRA-4567 Load EDW tables/ANALYTICS_DB/EDW/views/02_create_view_vw_dim_store_sales.sql
+++ changelog/JIRA-4567 Load EDW tables/ANALYTICS_DB/EDW/views/02_create_view_vw_dim_store_sales.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset nivethidhas:2
+--changeset nivethidhas:JIRA-4567_ANALYTICS_DB-EDW-VW_DIM_STORE_SALES_07-02-2025_15:11
 
 USE DATABASE ANALYTICS_DB;
 USE SCHEMA EDW;
