e1a6e1835e738c16bda40d15e67cde066f34fd40
nive-thi-dha <Nivethidha.S@gds.ey.com>
2025-02-07 14:37:09 +0530

    Update 02_create_procedure_dim_store_sales.sql

--- changelog/JIRA-4567 Load EDW tables/ANALYTICS_DB/EDW/procedures/02_create_procedure_dim_store_sales.sql
+++ changelog/JIRA-4567 Load EDW tables/ANALYTICS_DB/EDW/procedures/02_create_procedure_dim_store_sales.sql
@@ -4,12 +4,25 @@
 USE DATABASE ANALYTICS_DB;
 USE SCHEMA EDW;
 
-CREATE OR REPLACE PROCEDURE DIM_STORE_SALES
-RETURNS TABLE (
-SS_ITEM_SK NUMBER(38,0),
-SS_CUSTOMER_SK NUMBER(38,0)
-)
+CREATE OR REPLACE PROCEDURE DIM_STORE_SALES()
+RETURNS TABLE (SS_ITEM_SK NUMBER(38,0), SS_CUSTOMER_SK NUMBER(38,0))
+LANGUAGE JAVASCRIPT
 AS
-SELECT SS_ITEM_SK,SS_CUSTOMER_SK FROM VW_DIM_STORE_SALES;
+$$
+var result = [];
+var sql_command = "SELECT SS_ITEM_SK, SS_CUSTOMER_SK FROM VW_DIM_STORE_SALES";
+var statement1 = snowflake.createStatement({sqlText: sql_command});
+var result_set = statement1.execute();
+
+while (result_set.next()) {
+    result.push({
+        SS_ITEM_SK: result_set.getColumnValue('SS_ITEM_SK'),
+        SS_CUSTOMER_SK: result_set.getColumnValue('SS_CUSTOMER_SK')
+    });
+}
+
+return result;
+$$;
+
 
 --rollback DROP PROCEDURE DIM_STORE_SALES;
