8c603ca99c5f93f39c92c44fcf11b9599105cffe
nive-thi-dha <Nivethidha.S@gds.ey.com>
2025-02-07 14:33:46 +0530

    Update 01_create_procedure_dim_item.sql

--- changelog/JIRA-4567 Load EDW tables/ANALYTICS_DB/EDW/procedures/01_create_procedure_dim_item.sql
+++ changelog/JIRA-4567 Load EDW tables/ANALYTICS_DB/EDW/procedures/01_create_procedure_dim_item.sql
@@ -4,13 +4,21 @@
 USE DATABASE ANALYTICS_DB;
 USE SCHEMA EDW;
 
-CREATE OR REPLACE PROCEDURE DIM_ITEM
-RETURNS TABLE (
-I_ITEM_SK NUMBER(38,0),
-I_ITEM_ID VARCHAR(16)
-)
+CREATE OR REPLACE PROCEDURE DIM_ITEM()
+RETURNS TABLE (I_ITEM_SK NUMBER(38,0), I_ITEM_ID VARCHAR(16))
+LANGUAGE JAVASCRIPT
 AS
-SELECT I_ITEM_SK,I_ITEM_ID FROM VW_DIM_ITEM;
+$$
+var result = [];
+var sql_command = "SELECT I_ITEM_SK, I_ITEM_ID FROM VW_DIM_ITEM";
+var statement1 = snowflake.createStatement({sqlText: sql_command});
+var result_set = statement1.execute();
+while (result_set.next()) {
+    result.push({I_ITEM_SK: result_set.getColumnValue('I_ITEM_SK'), I_ITEM_ID: result_set.getColumnValue('I_ITEM_ID')});
+}
+return result;
+$$;
+
 
 --rollback DROP PROCEDURE DIM_ITEM;
 
